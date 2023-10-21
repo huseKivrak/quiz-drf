20543b04fc5958e84df46c4ad9133f7756eb97bf
Huse Kivrak <huse7@pm.me>
2023-10-21 19:47:28 -0400

    example permission code for now

---
+++ profiles/permissions.py
@@ -0,0 +1,10 @@
+from rest_framework import permissions
+
+class IsTeacher(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.type == 'TEACHER'
+
+class IsStudent(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.type == 'STUDENT'
+
