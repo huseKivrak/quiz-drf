e078079c158cfb461bea13dfa8fc2c5f2356a0bf
Huse Kivrak <huse7@pm.me>
2023-10-21 19:46:47 -0400

    dj-rest-auth urls

--- project/urls.py
+++ project/urls.py
@@ -5,5 +5,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('core.api_urls', namespace='api')),
+    path('api/v1/', include('core.api_urls', namespace='api')),
+    path('auth/', include('dj_rest_auth.urls')),
+    path('auth/signup/', include('dj_rest_auth.registration.urls')),
 ]
