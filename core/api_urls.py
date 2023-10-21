bc16a18a85f04deae1a3b9b1c7a247007909bcc4
Huse Kivrak <huse7@pm.me>
2023-10-21 19:47:07 -0400

    moved auth urls to project/urls.py

--- core/api_urls.py
+++ core/api_urls.py
@@ -1,13 +1,7 @@
 from django.urls import path, include
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
 
 app_name = 'api'
 
 urlpatterns = [
     path('', include('quizzes.urls')),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
